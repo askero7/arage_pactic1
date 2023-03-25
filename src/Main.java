9f5bf1f26e930e204586143f28dd800a121a25a1
Askatbek Zhunusaliuulu <askatbek.zhunusaliuulu.alt@gmail.com>
2023-03-25 21:40:05 +0300

    sf

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,18 @@ import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        String a = "  I like Java!!!";
-        System.out.println(a.charAt(a.length()-1));
-        System.out.println(true);
-        System.out.println(false);
-        System.out.println(true);
-        System.out.println(a.replace("a", "o"));
-        System.out.println(a.toUpperCase(Locale.ROOT));
-        System.out.println(a.toLowerCase(Locale.ROOT));
-        System.out.println(a.length());
-        String w = a.substring(9,13);
-        System.out.println(w.toUpperCase());
-        System.out.println(a.trim());
+//        String a = "  I like Java!!!";
+//        System.out.println(a.charAt(a.length()-1));
+//        System.out.println(true);
+//        System.out.println(false);
+//        System.out.println(true);
+//        System.out.println(a.replace("a", "o"));
+//        System.out.println(a.toUpperCase(Locale.ROOT));
+//        System.out.println(a.toLowerCase(Locale.ROOT));
+//        System.out.println(a.length());
+//        String w = a.substring(9,13);
+//        System.out.println(w.toUpperCase());
+//        System.out.println(a.trim());
     }
 //    Өзгөрүү болду
-
 }
